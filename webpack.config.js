5f8fd4f8e1e88a16c94ce5592ee9547da4e35506
Kevin Beam <14251936+kbeamnsidc@users.noreply.github.com>
2021-06-16 09:50:32 -0600

    Add TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
  module.exports = {
-   entry: './src/index.js',
+   entry: './src/index.ts',
    output: {
      filename: 'bundle.js',
      path: path.resolve(__dirname, 'dist'),
@@ -9,10 +9,17 @@ const path = require('path');
 
   module: {
     rules: [
-      {
+     {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }, {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
  };
